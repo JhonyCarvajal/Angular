b1f0bafb522412ad6020609d40eccf670f86854b
JhonyCarvajal <jhony150209@gmail.com>
2019-08-25 22:41:57 -0500

    commit

--- proyect/src/app/app.module.ts
+++ proyect/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NadvarComponent } from './components/shared/nadvar/nadvar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NadvarComponent
   ],
   imports: [
     BrowserModule
---
+++ proyect/src/app/components/shared/nadvar/nadvar.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NadvarComponent } from './nadvar.component';
+
+describe('NadvarComponent', () => {
+  let component: NadvarComponent;
+  let fixture: ComponentFixture<NadvarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NadvarComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NadvarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
